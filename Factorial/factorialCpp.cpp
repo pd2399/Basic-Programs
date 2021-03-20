7ead2fc5fc01877d1ca3c82d9ebad8cd8d57f493
pd2399 <nsk.pranav@gmail.com>
2021-03-20 20:35:02 +0530

    Added C+ program for Factorial

---
+++ Factorial/factorialCpp.cpp
@@ -0,0 +1,41 @@
+// Factorial of a number is the multiplication of all the numbers less than or equal to it.
+// For this, recursion is used
+
+#include <iostream>
+
+using namespace std;
+
+// Define a function to find the factorial
+int fact(int number){
+    if (number == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return (number*fact(number-1));
+    }
+    
+}
+
+void main(){      // Main Function
+    // Create a loop to check program continuously 
+    while (true)
+    {
+        // Take input from user
+        cout<< "Enter 0 to quit.\n";
+        cout<<"Enter the number: ";
+        int number;
+        cin>>number;
+        if (number == 0)
+        {
+            // Break out of loop
+            break;
+        }
+        else
+        {
+            // Call the above function
+            cout<<"The answer is: "<<fact(number)<<endl;
+        }
+    }
+}
